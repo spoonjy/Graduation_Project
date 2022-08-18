a5181a581aa455f6b174ef0a09dd13519342cc3c
spoonjy <yaroslav.lukashevich90@gmail.com>
2022-08-18 20:38:00 +0300

    API tests added

--- SwaggerPetstore/test_swagger_petstore.py
+++ SwaggerPetstore/test_swagger_petstore.py
@@ -4,7 +4,7 @@ from .swagger_petstore import *
 
 class TestApiPetStore:
     @allure.story('Add new pet and delete from store')
-    def test_pet(self):
+    def test_add__pet_and_delete_pet_from_store(self):
         with allure.step('Adding new pet with id 3'):
             post_add_new_pet(3, 'duck')
         with allure.step('Checking if a pet has been added with id 3'):
@@ -15,7 +15,7 @@ class TestApiPetStore:
             verify_del_new_pet(3)
 
     @allure.story('Create user, change first name and last name')
-    def test_user(self):
+    def test_add_user_and_change_his_first_name_and_last_name(self):
         with allure.step('Creating new user'):
             post_new_user(3, "Rick", "Yaroslav", "Lukashevich", "Yar@gmail.com", "123321", "+375331234567")
         with allure.step('Getting user data'):
